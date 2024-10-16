be6640f78adb15d9f82a1ac67ff34451f7a217d5
Roberto Tores <rocatoro18@outlook.com>
2024-10-16 16:51:00 -0700

    .

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeroesService } from './../../services/heroes.service';
+import { Component, OnInit } from '@angular/core';
+import { delay, switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styles: ``
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit{
+
+  public hero?: Hero;
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    // LEER EL URL
+    this.activatedRoute.params
+      .pipe(
+        delay(1000),
+        switchMap(({id}) => this.heroesService.getHeroById(id)),
+      ).subscribe(hero => {
+
+        // SI NO VIENE EL HEROE SE SACA A LA PERSONA DE ESTA PANTALLA
+        if(!hero) return  this.router.navigate(['/heroes/list']);
+
+        this.hero = hero;
+        //console.log({hero});
+        return;
+      })
+  }
+
+  goBack():void {
+    this.router.navigateByUrl('heroes/list');
+  }
 
 }
